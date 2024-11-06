1118cb88c88bfea218ce58440f9ca0d3dfeca1e9
divya1993pratap <divya1993pratap@hotmail.com>
2024-11-06 15:48:19 +0530

    New RG added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "name" {
     name = "Myrg12"
     location = "westus"  
 }
+
+resource "azurerm_resource_group" "name1" {
+    name = "Myrg123"
+    location = "eastus"  
+}
